6ddf3aad1fa9eb82bc2b5950edd973f9279ba3da
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-10-01 16:51:58 -0500

    Home page Fixes #2

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { useState } from 'react';
+import { useState} from 'react';
 
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, St } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 import HomeScreen from './components/HomeScreen';
 import MapScreen from './components/MapScreen';
@@ -13,16 +15,13 @@ export default function App() {
   const [schedule, useSchedule ] = useState()
 
   return (
-    <View style={styles.container}>
-      <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen}/>
-        <Stack.Screen name="Map" component={MapScreen} />
-        <Stack.Screen name="Schedule" component={ScheduleScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer style={styles.container}>
+        <Stack.Navigator >
+          <Stack.Screen name="Home" component={HomeScreen}/>
+          <Stack.Screen name="Map" component={MapScreen} />
+          <Stack.Screen name="Schedule" component={ScheduleScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,7 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { useState } from 'react';
 
-export default function HomeScreen()
+export default function HomeScreen({ navigation })
 {
-
+    return (
+        <View style={styles.container}>
+            <Button
+          title="Edit Schedule"
+          onPress={() =>
+            navigation.navigate('Schedule', { })
+          }
+        />
+        <Button
+          title="View Map"
+          onPress={() =>
+            navigation.navigate('Map', { })
+          }
+        />
+        </View>
+      );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
